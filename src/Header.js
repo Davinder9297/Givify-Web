ac71a83493a6e289737964c9230a6c0bd22e3ffd
Jaspreet Kaur <jaspreetkaursaini469@gmail.com>
2023-10-09 15:34:46 +0530

    Homepage created by jaspreet

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div>
+            <div className="bg-neutral-300 flex justify-between shadow-md shadow-neutral-400">
+                <div className="">
+                    <img className="h-16 ml-5 py-1" src="logo.png" alt="Givify" />
+                </div>
+                <div className="flex justify-center mr-6 ">
+                    <div className="py-5 px-5 hover:bg-neutral-400 cursor-pointer rounded-md hover:text-white mx-3 m-auto">About Us</div>
+                    <div className="py-5 px-5 hover:bg-neutral-400 cursor-pointer rounded-md hover:text-white mx-3 m-auto">Gallery</div>
+                    <div className="py-5 px-5 hover:bg-neutral-400 cursor-pointer rounded-md hover:text-white mx-3 m-auto">Our Partners</div>
+                    <div className="py-5 px-5 hover:bg-neutral-400 cursor-pointer rounded-md hover:text-white mx-3 m-auto">Contact Us</div>
+                    <div className="py-5 px-5 hover:bg-neutral-400 cursor-pointer rounded-md hover:text-white mx-3 m-auto">Join Us</div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/Videoadded.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export default function Videoadded()
+{
+    return(
+        <div className='w-[100%] border-2 border-solid border-red-800 min-h-[60vh] '>
+            <video autoPlay={true} muted loop>
+  <source src="https://www.kapwing.com/videos/6523ad13f66e2f63957aefd7" type="video/mp4"/>
+</video>
+{/* <img width='100%' height='20%' src="/mainvideo.gif" alt="" /> */}
+        </div>
+    )
+}
