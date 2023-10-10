5735053f329a04d04ec5f6307667551530c74b2f
Davinder9297 <davindergiri765@gmail.com>
2023-10-10 10:06:26 +0530

    Routing (davinder)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Header from './Header';
+import Home from './Home';
+import Try from './Try';
 import Videoadded from './Videoadded';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App h-screen">
-      <Header/>
-      <Videoadded/>
+
+       <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      
+          <Route path="/try" element={<Try />} />
+     
+      </Routes>
+    </BrowserRouter>
+  
       {/* <Videoadded/> */}
 
     </div>
