ea158f844713e2611f9a9101c09893c193b9b421
Jashanjot <kaurjashanjot16@gmail.com>
2023-10-11 07:40:26 +0530

    Saving changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Try from './Try';
+import About_us from './About_us';
+import Donate_us from './Donate_us';
+
+
+
 import Videoadded from './Videoadded';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
@@ -12,7 +17,11 @@ function App() {
       <Routes>
       <Route path="/" element={<Home/>}/>
       
-          <Route path="/try" element={<Try />} />
+          <Route path="/try" element={< Try/>} />
+          <Route path="/About" element={< About_us/>} />
+          <Route path="/donate_us" element={< Donate_us/>} />
+
+
      
       </Routes>
     </BrowserRouter>
